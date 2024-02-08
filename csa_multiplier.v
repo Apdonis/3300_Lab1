d5afc6698db149393975f6820c95490c4bbe4b45
Apdonis <158244629+Apdonis@users.noreply.github.com>
2024-02-08 12:06:20 -0800

    Add files via upload

--- csa_multiplier.v
+++ csa_multiplier.v
@@ -28,8 +28,8 @@ module csa_multiplier
     output cout
     );
     wire [3:0] sum0, sum1, sum2,sum3,sum4;
-    wire cout0, cout1, cout2, cout3, cout4,cout5,cout6,cout7, cout8, cout9,cout10;
-    wire  fasum1, fasum2,fasum3,fasum5,fasum6,fasum7;
+    wire facout[10:0];
+    wire  fasum[5:0];
     
     
     mq_4bit mq0 (.m(m[3:0]), .q(q[0]), .mq(sum0[3:0]));
@@ -39,20 +39,20 @@ module csa_multiplier
     
     assign sum[0] = m[0] & q[0]; 
     
-    full_adder fa0(.x(sum0[1]), .y(sum1[0]), .c_in(1'b0), .s(sum[1]), .c_out(cout0));
-    full_adder fa1(.x(sum0[2]), .y(sum1[1]), .c_in(sum2[0]), .s(fasum1), .c_out(cout1));
-    full_adder fa2(.x(sum0[3]), .y(sum1[2]), .c_in(sum2[1]), .s(fasum2), .c_out(cout2));
-    full_adder fa3(.x(1'b0), .y(sum1[3]), .c_in(sum2[2]), .s(fasum3), .c_out(cout3));
+    full_adder fa0(.x(sum0[1]), .y(sum1[0]), .c_in(1'b0), .s(sum[1]), .c_out(facout[0]));
+    full_adder fa1(.x(sum0[2]), .y(sum1[1]), .c_in(sum2[0]), .s(fasum[0]), .c_out(facout[1]));
+    full_adder fa2(.x(sum0[3]), .y(sum1[2]), .c_in(sum2[1]), .s(fasum[1]), .c_out(facout[2]));
+    full_adder fa3(.x(1'b0), .y(sum1[3]), .c_in(sum2[2]), .s(fasum[2]), .c_out(facout[3]));
     
-    full_adder fa4(.x(1'b0), .y(fasum1), .c_in(cout0), .s(sum[2]), .c_out(cout4));
-    full_adder fa5(.x(fasum2), .y(sum3[0]), .c_in(cout1), .s(fasum5), .c_out(cout5));
-    full_adder fa6(.x(fasum3), .y(sum3[1]), .c_in(cout2), .s(fasum6), .c_out(cout6));
-    full_adder fa7(.x(sum3[2]), .y(sum2[3]), .c_in(cout3), .s(fasum7), .c_out(cout7));
+    full_adder fa4(.x(1'b0), .y(fasum[0]), .c_in(facout[0]), .s(sum[2]), .c_out(facout[4]));
+    full_adder fa5(.x(fasum[1]), .y(sum3[0]), .c_in(facout[1]), .s(fasum[3]), .c_out(facout[5]));
+    full_adder fa6(.x(fasum[2]), .y(sum3[1]), .c_in(facout[2]), .s(fasum[4]), .c_out(facout[6]));
+    full_adder fa7(.x(sum3[2]), .y(sum2[3]), .c_in(facout[3]), .s(fasum[5]), .c_out(facout[7]));
     
-    full_adder fa8(.x(fasum5), .y(cout4), .c_in(1'b0), .s(sum[3]), .c_out(cout8));
-    full_adder fa9(.x(fasum6), .y(cout5), .c_in(cout8), .s(sum[4]), .c_out(cout9));
-    full_adder fa10(.x(fasum7), .y(cout6), .c_in(cout9), .s(sum[5]), .c_out(cout10));
-    full_adder fa11(.x(cout7), .y(sum3[3]), .c_in(cout10), .s(sum[6]), .c_out(cout));
+    full_adder fa8(.x(fasum[3]), .y(facout[4]), .c_in(1'b0), .s(sum[3]), .c_out(facout[8]));
+    full_adder fa9(.x(fasum[4]), .y(facout[5]), .c_in(facout[8]), .s(sum[4]), .c_out(facout[9]));
+    full_adder fa10(.x(fasum[5]), .y(facout[6]), .c_in(facout[9]), .s(sum[5]), .c_out(facout[10]));
+    full_adder fa11(.x(facout[7]), .y(sum3[3]), .c_in(facout[10]), .s(sum[6]), .c_out(cout));
     
     assign sum[7] = cout;
     
